2867562f38aa8bc4af9e66083aeffe7301019e4b
Carol Chero Paz <cheropazpilar@gmail.com>
2022-05-31 13:06:23 -0500

    cambios en test

--- test/methods.spec.js
+++ test/methods.spec.js
@@ -1,159 +1,171 @@
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
 const {
     getStatusCode,
     getTextFileHref, searchFilesOrDirectory, getPropertiesOfObject,
     validityStatusCode, getLinks,
     getStatsUniqueBroken, getStatsUnique,
-  } = require("../src/methods");
+// eslint-disable-next-line quotes
+} = require("../src/methods");
   
-  jest.mock("chalk", () => ({
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line quotes
+jest.mock("chalk", () => ({
+    // eslint-disable-next-line quotes
     green: jest.fn(() => "green"),
+    // eslint-disable-next-line quotes
     yellow: jest.fn(() => "yellow"),
+    // eslint-disable-next-line quotes
     red: jest.fn(() => "red"),
+    // eslint-disable-next-line quotes
     yellowBright: jest.fn(() => "yellowBright"),
+    // eslint-disable-next-line quotes
     blue: jest.fn(() => "blue"),
+    // eslint-disable-next-line quotes
     bgRed: jest.fn(() => "bgRed"),
     bold: {
-      redBright: jest.fn(() => "redBright"),
-      magenta: jest.fn(() => "magenta"),
+    // eslint-disable-next-line quotes
+        redBright: jest.fn(() => "redBright"),
+        // eslint-disable-next-line quotes
+        magenta: jest.fn(() => "magenta"),
     },
-  }));
+}));
   
-  describe("searchFilesOrDirectory", () => {
+describe("searchFilesOrDirectory", () => {
     it("if pathUser is a file return a file markdown", () => {
-      const pathUser = "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md";
-      expect(searchFilesOrDirectory(pathUser, [])).toEqual(["D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md"]);
+        const pathUser = "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md";
+        expect(searchFilesOrDirectory(pathUser, [])).toEqual(["D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md"]);
     });
     it("if pathUSer is a folder return a array's file markdown", () => {
-      const pathUser = "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md";
-      expect(searchFilesOrDirectory(pathUser, [])[0]).toBe("D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md");
+        const pathUser = "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md";
+        expect(searchFilesOrDirectory(pathUser, [])[0]).toBe("D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md");
     });
-  });
+});
   
-  describe("getTextFileHref", () => {
+describe("getTextFileHref", () => {
     it("Return an object array with file, href and text(text with less a 50 characters)", () => {
-      const arrayFiles = [
-        "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-      ];
-      expect(getTextFileHref(arrayFiles)).toEqual([{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://docs.npmjs.com/cli/install",
-        text: "docs oficiales de `npm install` acá",
-      }, {
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://github.com/Laboratoria/course-parser",
-        text: "`course-parser`",
-      }]);
+        const arrayFiles = [
+            "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+        ];
+        expect(getTextFileHref(arrayFiles)).toEqual([{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://docs.npmjs.com/cli/install",
+            text: "docs oficiales de `npm install` acá",
+        }, {
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://github.com/Laboratoria/course-parser",
+            text: "`course-parser`",
+        }]);
     });
-  });
+});
   
-  describe("getStatusCode", () => {
+describe("getStatusCode", () => {
     it("Return a number of status code", () => {
-      const link = "https://es.wikipedia.org/wiki/Markdown";
-      getStatusCode(link).then((n) => {
-        expect(n).toBe(200);
-      });
+        const link = "https://es.wikipedia.org/wiki/Markdown";
+        getStatusCode(link).then((n) => {
+            expect(n).toBe(200);
+        });
     });
     it("Return status code (https's protocol)", () => {
-      const link = "https://es.wikipedia.org/wiki/Markdown";
-      return expect(getStatusCode(link)).resolves.toBe(200);
+        const link = "https://es.wikipedia.org/wiki/Markdown";
+        return expect(getStatusCode(link)).resolves.toBe(200);
     });
     it("Return status code(http's protocol)", () => {
-      const link = "http://es.wikipedia.org/wiki";
-      return expect(getStatusCode(link)).resolves.toBe(301);
+        const link = "http://es.wikipedia.org/wiki";
+        return expect(getStatusCode(link)).resolves.toBe(301);
     });
     it("Return a message when request fail (https's protocol)", () => {
-      const link = "https://fakecccaa1.com";
-      return expect(getStatusCode(link)).resolves.toBe("problem with request: getaddrinfo ENOTFOUND fakecccaa1.com on the link (https://fakecccaa1.com)");
+        const link = "https://fakecccaa1.com";
+        return expect(getStatusCode(link)).resolves.toBe("problem with request: getaddrinfo ENOTFOUND fakecccaa1.com on the link (https://fakecccaa1.com)");
     });
     it("Return a message when request fail (http's protocol)", () => {
-      const link = "http://fakecccaa1.com";
-      return expect(getStatusCode(link)).resolves.toBe("problem with request: getaddrinfo ENOTFOUND fakecccaa1.com on the link(http://fakecccaa1.com)");
+        const link = "http://fakecccaa1.com";
+        return expect(getStatusCode(link)).resolves.toBe("problem with request: getaddrinfo ENOTFOUND fakecccaa1.com on the link(http://fakecccaa1.com)");
     });
     it("Return a message when protocol is not http or https", () => {
-      const link = "ftp://fakecccaa1.com";
-      return expect(getStatusCode(link)).resolves.toBe("problem with request: this protocol's link ftp://fakecccaa1.com is not http or https");
+        const link = "ftp://fakecccaa1.com";
+        return expect(getStatusCode(link)).resolves.toBe("problem with request: this protocol's link ftp://fakecccaa1.com is not http or https");
     });
-  });
-  
-  describe("validityStatusCode", () => {
+});
+
+describe("validityStatusCode", () => {
     it("Return Ok due to a valid number", () => {
-      expect(validityStatusCode(200)).toBe("Ok");
-      expect(validityStatusCode(301)).toBe("Ok");
-      expect(validityStatusCode(302)).toBe("Ok");
+        expect(validityStatusCode(200)).toBe("Ok");
+        expect(validityStatusCode(301)).toBe("Ok");
+        expect(validityStatusCode(302)).toBe("Ok");
     });
     it("Return Fail due to a invalid number", () => {
-      expect(validityStatusCode(404)).toBe("Fail");
+        expect(validityStatusCode(404)).toBe("Fail");
     });
-  });
+});
   
-  describe("getPropertiesOfObject", () => {
+describe("getPropertiesOfObject", () => {
     it("Return an object array (file,href,text) when option is false", () => {
-      const route = "./examples/folder/directory1/readme6.md";
-      const optionTrueFalse = { validate: false };
-      return expect(getPropertiesOfObject(route, optionTrueFalse)).resolves.toEqual([{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://docs.npmjs.com/cli/install",
-        text: "docs oficiales de `npm install` acá"
-      }, {
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://github.com/Laboratoria/course-parser",
-        text: "`course-parser`",
-      }]);
+        const route = "./examples/folder/directory1/readme6.md";
+        const optionTrueFalse = { validate: false };
+        return expect(getPropertiesOfObject(route, optionTrueFalse)).resolves.toEqual([{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://docs.npmjs.com/cli/install",
+            text: "docs oficiales de `npm install` acá"
+        }, {
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://github.com/Laboratoria/course-parser",
+            text: "`course-parser`",}]);
     });
     it("Return an object array (file,href,text,statusCode,status) when option is true", () => {
-      const route = "./examples/folder/directory1/readme6.md";
-      const optionTrueFalse = { validate: true };
-      return expect(getPropertiesOfObject(route, optionTrueFalse)).resolves.toEqual([{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://docs.npmjs.com/cli/install",
-        text: "docs oficiales de `npm install` acá",
-        statusCode: 301,
-        status: "ok",
-      }, {
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://github.com/Laboratoria/course-parser",
-        text: "`course-parser`",
-        statusCode: 301,
-        status: "ok",
-      }]);
+        const route = "./examples/folder/directory1/readme6.md";
+        const optionTrueFalse = { validate: true };
+        return expect(getPropertiesOfObject(route, optionTrueFalse)).resolves.toEqual([{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://docs.npmjs.com/cli/install",
+            text: "docs oficiales de `npm install` acá",
+            statusCode: 301,
+            status: "ok",
+        }, {
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://github.com/Laboratoria/course-parser",
+            text: "`course-parser`",
+            statusCode: 301,
+            status: "ok",
+        }]);
     });
-  });
+});
   
-  describe("getLinks", () => {
+describe("getLinks", () => {
     it("Return a number of unique link", () => {
-      const arrayObject = [
-        { href: "https://developers.google.com/v8/" },
-        { href: "https://developers.google.com/v8/" },
-        { href: "https://docs.npmjs.com/cli/install/" }];
-      expect(getLinks(arrayObject)).toBe(2);
+        const arrayObject = [
+            { href: "https://developers.google.com/v8/" },
+            { href: "https://developers.google.com/v8/" },
+            { href: "https://docs.npmjs.com/cli/install/" }];
+        expect(getLinks(arrayObject)).toBe(2);
     });
-  });
+});
   
-  describe("getStatsUniqueBroken", () => {
+describe("getStatsUniqueBroken", () => {
     it("Return an object array with data total and unique and broken", () => {
-      const arrayObject = [{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://docs.npmjs.com/cli/install",
-        text: "docs oficiales de `npm install` acá",
-        status: "Fail",
-      }, {
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://github.com/Laboratoria/course-parser",
-        text: "`course-parser`",
-        statusCode: 301,
-        status: "Ok",
-      }];
-      expect(getStatsUniqueBroken(arrayObject)).toEqual([{ Unique: 2, total: 2, Broquen: 1 }]);
+        const arrayObject = [{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://docs.npmjs.com/cli/install",
+            text: "docs oficiales de `npm install` acá",
+            status: "Fail",
+        }, {
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://github.com/Laboratoria/course-parser",
+            text: "`course-parser`",
+            statusCode: 301,
+            status: "Ok",
+        }];
+        expect(getStatsUniqueBroken(arrayObject)).toEqual([{ Unique: 2, total: 2, Broquen: 1 }]);
     });
-  });
+});
   
-  describe("getStatsUnique", () => {
+describe("getStatsUnique", () => {
     it("Return an object array with data total and unique", () => {
-      const arrayObject = [{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://developers.google.com/v8/",
-        text: "motor de JavaScript V8 de Chrome",
-      }];
-      expect(getStatsUnique(arrayObject)).toEqual([{ Unique: 1, total: 1 }]);
+        const arrayObject = [{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
+            href: "https://developers.google.com/v8/",
+            text: "motor de JavaScript V8 de Chrome",
+        }];
+        expect(getStatsUnique(arrayObject)).toEqual([{ Unique: 1, total: 1 }]);
     });
-  });
+});
--- test/util.spec.js
+++ test/util.spec.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable quotes */
 const {
     printObject,
     printStatAndValidate,
     printObjectFalse,
     printObjectStats,
-  } = require("../src/util");
+} = require("../src/util");
   
-  jest.mock("chalk", () => ({
+jest.mock("chalk", () => ({
     green: jest.fn(() => "green"),
     yellow: jest.fn(() => "yellow"),
     red: jest.fn(() => "red"),
@@ -13,94 +15,95 @@ const {
     blue: jest.fn(() => "blue"),
     bgRed: jest.fn(() => "bgRed"),
     bold: {
-      redBright: jest.fn(() => "redBright"),
-      magenta: jest.fn(() => "magenta"),
+        redBright: jest.fn(() => "redBright"),
+        magenta: jest.fn(() => "magenta"),
     },
-  }));
-  describe("printObject", () => {
+}));
+describe("printObject", () => {
     it("Return a text with properties and colors", () => {
-      const arrayObject = [{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md\n",
-        href: "https://docs.npmjs.com/cli/install",
-        text: "docs oficiales de npm install acá más de 50 line",
-        statusCode: 404,
-        status: "Fail",
-      },
-      ];
-      const result = "*File:green Href:yellow Text:blue Status:redBright StatusCode:yellowBright\n";
-      expect(printObject(arrayObject)).toBe(result);
+        const arrayObject = [{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory\\file6.md\n",
+            href: "https://docs.npmjs.com/cli/install",
+            text: "docs oficiales de `npm install` acá",
+            statusCode: 404,
+            status: "Fail",
+        },
+        ];
+        const result = "*File:green Href:yellow Text:blue Status:redBright StatusCode:yellowBright\n";
+        expect(printObject(arrayObject)).toBe(result);
     });
     it("Return a text with properties and colors", () => {
-      const arrayObject = [{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://github.com/Laboratoria/course-parser",
-        text: "course-parser",
-        statusCode: 301,
-        status: "Ok",
-      }];
-      const result = "*File:green Href:yellow Text:blue Status:magenta StatusCode:yellowBright\n";
-      expect(printObject(arrayObject)).toBe(result);
-    });
-  });
-  
-  describe("printObjectFalse", () => {
-    it("Return a text with properties and colors", () => {
-      const arrayObject = [{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md\n",
-        href: "https://docs.npmjs.com/cli/install",
-        text: "docs oficiales de npm install acá más de 50 line",
-      },
-      ];
-      const result = "*File:green Href:yellow Text:blue\n";
-      expect(printObjectFalse(arrayObject)).toBe(result);
-    });
-    it("Return a text with properties and colors", () => {
-      const arrayObject = [{
-        file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-        href: "https://github.com/Laboratoria/course-parser",
-        text: "course-parser",
-      }];
-      const result = "*File:green Href:yellow Text:blue\n";
-      expect(printObjectFalse(arrayObject)).toBe(result);
-    });
-  });
-  describe("printStatAndValidate", () => {
-    it("Return a text with properties and colors", () => {
-      const arrayObject = [
-        {
-          file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-          href: "https://github.com/Laboratoria/course-parser",
-          text: "course-parser",
-          statusCode: 301,
-          status: "Ok",
+        const arrayObject = [{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory\\file6.md",
+            href: "https://github.com/Laboratoria/course-parser",
+            text: "course-parser",
+            statusCode: 301,
+            status: "Ok",
         }];
-      const result = `
-      --------------------
-         bgRed : yellowBright 
-         bgRed : yellowBright
-         bgRed : yellowBright
-      --------------------
+        const result = "*File:green Href:yellow Text:blue Status:magenta StatusCode:yellowBright\n";
+        expect(printObject(arrayObject)).toBe(result);
+    });
+});
+  
+describe("printObjectFalse", () => {
+    it("Return a text with properties and colors", () => {
+        const arrayObject = [{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory\\file6.md\n",
+            href: "https://docs.npmjs.com/cli/install",
+            text: "docs oficiales de `npm install` acá",
+        },
+        ];
+        const result = "*File:green Href:yellow Text:blue\n";
+        expect(printObjectFalse(arrayObject)).toBe(result);
+    });
+    it("Return a text with properties and colors", () => {
+        const arrayObject = [{
+            file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory\\file6.md",
+            href: "https://github.com/Laboratoria/course-parser",
+            text: "course-parser",
+        }];
+        const result = "*File:green Href:yellow Text:blue\n";
+        expect(printObjectFalse(arrayObject)).toBe(result);
+    });
+});
+describe("printStatAndValidate", () => {
+    it("Return a text with properties and colors", () => {
+        const arrayObject = [
+            {
+                file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory\\file6.md",
+                href: "https://github.com/Laboratoria/course-parser",
+                text: "course-parser",
+                statusCode: 301,
+                status: "Ok",
+            }];
+        
+        const result = `
+           --------------------
+              bgRed : yellowBright 
+              bgRed : yellowBright
+              bgRed : yellowBright
+           --------------------
       `;
-      expect(printStatAndValidate(arrayObject)).toBe(result);
+        expect(printStatAndValidate(arrayObject)).toBe(result);
     });
-  });
+});
   
-  describe("printObjectStats", () => {
+describe("printObjectStats", () => {
     it("Return a text with properties and colors", () => {
-      const arrayObject = [
-        {
-          file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory1\\readme6.md",
-          href: "https://github.com/Laboratoria/course-parser",
-          text: "course-parser",
-          statusCode: 301,
-          status: "Ok",
-        }];
-      const result = `
-    --------------------
-      bgRed : yellowBright 
-      bgRed : yellowBright
-    --------------------
+        const arrayObject = [
+            {
+                file: "D:\\Laboratoria\\md-links\\LIM17-md-links\\examples\\folder\\directory\\file6.md",
+                href: "https://github.com/Laboratoria/course-parser",
+                text: "course-parser",
+                statusCode: 301,
+                status: "Ok",
+            }];
+        const result = `
+       --------------------
+        bgRed : yellowBright 
+        bgRed : yellowBright
+       --------------------
     `;
-      expect(printObjectStats(arrayObject)).toBe(result);
+        expect(printObjectStats(arrayObject)).toBe(result);
     });
-  });
+});
